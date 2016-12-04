dcf0ef9d7f1b55c99e234585560d0674b108543f
matthewmjm <matthewmjm@yahoo.com>
2016-12-03 18:41:22 -0700

    finished exercise

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 function average(numbers) {
     // your code goes here
+
+    var total = numbers[0];
+    for (var i = 1; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total / numbers.length;
 }
+
 
 
 
